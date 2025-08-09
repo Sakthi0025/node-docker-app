ac7d16e7c5962a3db3f52b8f786bfba6887f5af1
root <root@ip-172-31-36-174.ec2.internal>
2025-08-09 16:00:51 +0000

    updated by sakthi

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+<<<<<<< HEAD
     res.send('jenkins automated deployment!');
+=======
+    res.send('Hello from CI/CD DEMO!');
+>>>>>>> 7df1422 (updated by sakthi)
 });
 
 const port = 3000;
